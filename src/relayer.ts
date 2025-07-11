391566d4ffdc668a0724f1a7a60bad04ab28b751
Johan Chacon Chacon <114323459+jjcc2000@users.noreply.github.com>
2025-07-11 10:33:25 -0600

    Update relayer.ts for the Production setup!

--- src/relayer.ts
+++ src/relayer.ts
@@ -5,10 +5,7 @@ dotenv.config();
 
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
-console.log("This is the tes");
 
-console.log("PRIVATE_KEY:", process.env.PRIVATE_KEY);
-console.log("RPC_URL:", process.env.RPC_URL);
 export const relayTransaction = async (to: string, data: string, gasLimit: string) => {
     const tx = {
         to,
